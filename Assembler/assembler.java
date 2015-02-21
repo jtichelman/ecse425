5c86529dc16ea8fe490b93e291bcc23dd1e607b9
jtichelman <jeffrey.tichelman@mail.mcgill.ca>
2015-02-21 13:24:05 -0500

    Begin Encoding
    -case statements for all instructions
    -calculation of argument ints
    -added some assembly programs to folder
    -can't support inline tabbing due to bug in String.replace
    
    TODO:
    -branching and memory argument calculation
    -binary encoding
    -fix tab bug (?)

--- Assembler/assembler.java
+++ Assembler/assembler.java
@@ -19,7 +19,7 @@ public class assembler{
 			File outputFile = new File("output.txt");
 			FileWriter fileWriter = new FileWriter(outputFile);
 			BufferedWriter writeBuffer = new BufferedWriter(fileWriter);
-			
+
 			//Have to mark bufferedReader to reset later
 			//argument is readAhead limit, set to sufficiently large value
 			readBuffer.mark(50000);							
@@ -47,7 +47,6 @@ public class assembler{
 			while((line = readBuffer.readLine()) != null) {
 				if(!isEmpty(line)){
 					lineCounter++;
-					
 					//gets instructions in ArrayList format
 					ArrayList<String> instruction = delimitInstruction(line);
 					//for parsing list
@@ -55,12 +54,184 @@ public class assembler{
 					
 					//TODO: Based on the instruction entered calculate the appropriate arguments
 					
+					//Gets first item in list (the operation)
+					String operation = li.next().toString();
+					writeBuffer.write(operation + " ");
+					
+					switch(operation){
+					case "add":
+						int d = getIntFromRegister(li.next().toString(), false);
+						int s = getIntFromRegister(li.next().toString(), false);
+						int t = getIntFromRegister(li.next().toString(), true);	
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+						
+					case "sub":
+						d = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);	
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+					
+					case "addi":
+						t = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						int i = Integer.parseInt(li.next().toString());
+						writeBuffer.write(t + " " + s + " " + i);
+						break;
+						
+					case "mult":
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);
+						writeBuffer.write(s + " " + t);
+						break;
+						
+					case "div":
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), false);
+						writeBuffer.write(s + " " + t);
+						break;
+						
+					case "slt":
+						d = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);	
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+						
+					case "slti":
+						t = getIntFromRegister(li.next().toString(), false);	
+						s = getIntFromRegister(li.next().toString(), false);
+						i = Integer.parseInt(li.next().toString());
+						writeBuffer.write(t + " " + s + " " + i);
+						break;
+						
+					case "and":
+						d = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);	
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+						
+					case "or":
+						d = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);	
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+						
+					case "nor":
+						d = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);	
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+						
+					case "xor":
+						d = getIntFromRegister(li.next().toString(), false);
+						s = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), true);
+						writeBuffer.write(d + " " + s + " " + t);
+						break;
+						
+					case "andi":
+						t = getIntFromRegister(li.next().toString(), false);	
+						s = getIntFromRegister(li.next().toString(), false);
+						i = Integer.parseInt(li.next().toString());
+						writeBuffer.write(t + " " + s + " " + i);
+						break;
+						
+					case "ori":
+						t = getIntFromRegister(li.next().toString(), false);	
+						s = getIntFromRegister(li.next().toString(), false);
+						i = Integer.parseInt(li.next().toString());
+						writeBuffer.write(t + " " + s + " " + i);
+						break;
+						
+					case "xori":
+						t = getIntFromRegister(li.next().toString(), true);	
+						s = getIntFromRegister(li.next().toString(), false);
+						i = Integer.parseInt(li.next().toString());
+						writeBuffer.write(t + " " + s + " " + i);
+						break;
+						
+					case "mfhi":
+						d = getIntFromRegister(li.next().toString(), true);
+						writeBuffer.write(d);
+						break;
+						
+					case "mflo":
+						d = getIntFromRegister(li.next().toString(),true);
+						writeBuffer.write(String.valueOf(d));
+						break;
+						
+					case "lui":
+						t = getIntFromRegister(li.next().toString(), true);
+						i = Integer.parseInt(li.next().toString());
+						writeBuffer.write(t + " " + i);
+						break;
+						
+					case "sll":
+						d = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), false);
+						int h = Integer.parseInt(li.next().toString());
+						writeBuffer.write(d + " " + t + " " + h);
+						break;
+						
+					case "slr":
+						d = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), false);
+						h = Integer.parseInt(li.next().toString());
+						writeBuffer.write(d + " " + t + " " + h);
+						break;
+						
+					case "sra":
+						d = getIntFromRegister(li.next().toString(), false);
+						t = getIntFromRegister(li.next().toString(), false);
+						h = Integer.parseInt(li.next().toString());
+						writeBuffer.write(d + " " + t + " " + h);
+						break;
+						
+					//Memory instructions will have to calculate offsets
+					case "lw":
+						break;
+						
+					case "lb":
+						break;
+						
+					case "sw":
+						break;
+						
+					case "sb":
+						break;
+						
+					//Branch instructions need to find label line and calculate offset
+					case "beq":
+						break;
+						
+					case "bne":
+						break;
+						
+					case "j":
+						break;
+						
+					case "jr":
+						break;
+						
+					case "jal":
+						break;		
+						
+					//If none of these cases are met the instruction is invalid	
+					default:
+						System.out.println("Instruction " + operation + " not recognized");
+						break;
+					}	
 					
 					//writes list to output file
-					while(li.hasNext()){
+/*					while(li.hasNext()){
 						writeBuffer.write(li.next().toString());
 						writeBuffer.write(" ");
-					}
+					}*/
 					writeBuffer.newLine();
 				}
 			}				
@@ -70,8 +241,6 @@ public class assembler{
 			fileWriter.close();
 			fileReader.close();
 			readBuffer.close();
-			
-			System.out.println(lineCounter);
 			
 			//The below instructions simply open the written output file after execution
 			Desktop dt = Desktop.getDesktop();
@@ -84,11 +253,11 @@ public class assembler{
 	
 	// Line is empty if there's no characters on it or if it begins with '#'
 	public static boolean isEmpty(String str){
-		if(str.isEmpty())													//if string is empty then line is empty
-			return true;
 		while(str.startsWith(" ") || str.startsWith("\t")){					//get rid of empty space at beginning of string
 			str = str.substring(1);
 		}
+		if(str.isEmpty())													//if string is empty then line is empty
+			return true;
 		if(str.startsWith("#"))											//if string begins with #, line is a commment and should be ignored
 			return true;
 		return false;
@@ -105,6 +274,13 @@ public class assembler{
 		while(s.startsWith(" ") || s.startsWith("\t")){					//get rid of empty space at beginning of string
 			s = s.substring(1);
 		}
+		
+		//Try to remove tabs, seems to be a bug with Stirng.replace that won't allow you to replace "\t"
+		if(s.contains("\t")){
+			int tabIndex = s.indexOf("\t");
+			s=s.substring(0, tabIndex);	
+		}
+		System.out.println(s);
 		try{															//delimit string by spaces
 			String[] str = s.split(" ");
 			for(int i =0; i<str.length; i++){
@@ -118,6 +294,20 @@ public class assembler{
 			e.printStackTrace();
 		}
 		return instruction;											//return list
+	}
+	
+	public static int getIntFromRegister(String s, boolean isLast){
+		if(isLast){
+			int dollarIndex = s.indexOf("$");
+			s = s.substring(dollarIndex+1);
+			return Integer.parseInt(s);
+		}
+		else {
+			int dollarIndex = s.indexOf("$");
+			int commaIndex = s.indexOf(",");
+			s = s.substring(dollarIndex+1, commaIndex);
+			return Integer.parseInt(s);
+		}
 	}
 }
 
