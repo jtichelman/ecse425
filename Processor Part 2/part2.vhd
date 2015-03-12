7fc47a3325271beb70e2ff3a4ddb9ca37ba7a08c
Henry <henrylu93@hotmail.com>
2015-03-12 15:52:59 -0400

    Connected all the stages, added a memory controller

--- Processor Part 2/part2.vhd
+++ Processor Part 2/part2.vhd
@@ -25,23 +25,24 @@ end part2;
 
 Architecture implementation of part2 is
 	--Component declarations
+	--Fetch Stage
 	Component fetch_stage is 
 		Port (
 			clock: in std_logic;
-			
+			fetch_en : in std_logic;
 			--Memory ports
 			address_mem: out integer;
-			word_byte_mem : out std_logic;
-			write_en : out std_logic;
-			write_done : in std_logic;
+--			word_byte_mem : out std_logic;
+--			write_en : out std_logic;
+--			write_done : in std_logic;
 			read_en : out std_logic;
 			read_ready : in std_logic;
 			data_mem : inout std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0);
 			
 			--Branch ports
 			pc_in : in integer;
-			instruction_in : in std_logic_vector(31 downto 0);
-			branch_check : in std_logic;
+--			instruction_in : in std_logic_vector(31 downto 0);
+--			branch_check : in std_logic;
 			
 			--Output ports
 			instruction_out : out std_logic_vector(31 downto 0);
@@ -50,7 +51,6 @@ Architecture implementation of part2 is
 	End component;
 	
 	--ID stage
-	
 	Component instruction_decode is
 		port( 	
 			instruction		:	in std_logic_vector(31 downto 0);
@@ -65,7 +65,6 @@ Architecture implementation of part2 is
 	END component;
 		
 	--EX stage
-	
 	Component execute is
 		port( 
 			enable			: in std_logic;
@@ -84,8 +83,8 @@ Architecture implementation of part2 is
 	  );
 	  
 	 end component; 
-	--MEM stage
 	
+	--MEM stage
 	Component mem is
 		PORT (	
 			CLK : in std_logic;
@@ -96,7 +95,7 @@ Architecture implementation of part2 is
 			COND : in std_logic;
 			READ_READY, WRITE_DONE : in std_logic;
 			NPC : in integer;
-			INST : in integer;
+			INSTRUCTION : in std_logic_vector(5 downto 0);
 			PC : out integer;
 			READ_EN, WRITE_EN, WORD_BYTE_MEM : out std_logic;
 			ADDRESS_MEM : out integer;
@@ -105,7 +104,6 @@ Architecture implementation of part2 is
 	end component;
 	
 	--WB stage
-	
 	Component writeback_stage is
 		Generic (
 		Mem_Size_in_Word : integer:=1024;	
@@ -116,18 +114,154 @@ Architecture implementation of part2 is
 	);
 		Port (
 		clock : in std_logic;
+		write_back_en : in std_logic;
 		from_mem : in std_logic_vector (31 downto 0);
 		from_alu : in std_logic_vector (31 downto 0);
 		instruction: in std_logic_vector (31 downto 0);
 		
 		write_data : out std_logic_vector (31 downto 0);
-		reg_address : out integer range 0 to 31;
-		reg_enable : in std_logic
+		reg_address : out std_logic_vector (4 downto 0);
+		reg_enable : out std_logic
 	);
 	end component;
+	
+	--Main memory component
+	Component Main_memory is
+		generic (
+				File_Address_Read : string :="Init.dat";
+				File_Address_Write : string :="MemCon.dat";
+				Mem_Size_in_Word : integer:=1024;	
+				Num_Bytes_in_Word: integer:=4;
+				Num_Bits_in_Byte: integer := 8; 
+				Read_Delay: integer:=0; 
+				Write_Delay:integer:=0
+			 );
+		port (
+				clk : in std_logic;
+				address : in integer;
+				Word_Byte: in std_logic; -- when '1' you are interacting with the memory in word otherwise in byte
+				we : in std_logic;
+				wr_done:out std_logic; --indicates that the write operation has been done.
+				re :in std_logic;
+				rd_ready: out std_logic; --indicates that the read data is ready at the output.
+				data : inout std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0);        
+				initialize: in std_logic;
+				dump: in std_logic
+			 );		
+	End Component;
+	
+	--Memory controller component
+	component mem_controller is
+		Port(
+			clock : in std_logic;
+			
+			--Ports from IF stage
+			address_if : in integer;
+			read_en_if : in std_logic;
+			rd_ready_if : out std_logic;
+			data_if : inout std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0);  
+			
+			--Ports from MEM stage
+			address_mem : in integer;
+			wordbyte_mem : in std_logic;
+			write_en_mem : in std_logic;
+			read_en_mem : in std_logic;
+			wr_done_mem : out std_logic;
+			rd_read_mem : out std_logic;
+			data_mem : inout std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0); 
+		
+			--Ports to main memory
+			address_out : out integer;
+			word_byte_out : out std_logic;
+			write_en_out : out std_logic;
+			wr_done_in : in std_logic;
+			read_en_out : out std_logic;
+			rd_ready_in : in std_logic;
+			data_inout : inout std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0)
+		);
+	end component;
+	
 	--Signal Declarations
-	--Connecting stages together
+	signal init_signal : std_logic;
+	signal dump_signal: std_logic;
+	
+	--Fetch Signals
+	signal fetch_enable : std_logic;
+	signal pc_in : integer := 0;
+	signal instruction_if : std_logic_vector(31 downto 0);
+	signal npc_if: integer;
+	signal address_if : integer;
+	signal read_en_if : std_logic;
+	signal rd_ready_if : std_logic;
+	signal data_if : std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0);  
+	
+	-- Decode signals
+	signal wb_address : std_logic_vector(4 downto 0);
+	signal wb_data : std_logic_vector(31 downto 0);
+	signal wb_en : std_logic;
+	signal decode_en : std_logic;
+	signal op_code : std_logic_vector(5 downto 0);
+	signal d_reg, shift : std_logic_vector(4 downto 0);
+	signal address : std_logic_vector(25 downto 0);
+	signal s_reg, t_reg, imm : std_logic_vector(31 downto 0);
+	
+	-- Execute signals
+	signal execute_en, branch_cond : std_logic;
+	signal alu_out, b_operand : std_logic_vector(31 downto 0);
+	
+	-- Memory signals
+	signal mem_en : std_logic;
+	signal instruction : integer;
+	signal npc_mem : integer;
+	signal mem_output : std_logic_vector(31 downto 0);
+	signal address_mem : integer;
+	signal data_mem : std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0); 
+	signal rd_ready_mem : std_logic;
+	signal wr_done_mem : std_logic;
+	signal read_en_mem : std_logic;
+	signal write_en_mem : std_logic;
+	signal wordbyte_mem : std_logic;
+	
+	-- Writeback signals
+	signal write_back_enable : std_logic;
+	signal write_data : std_logic_vector(31 downto 0);
+	signal reg_address : std_logic_vector (4 downto 0);
+	
+	--Memory controler signals
+	signal address_out : integer;
+	signal word_byte_out : std_logic;
+	signal write_en_out : std_logic;
+	signal wr_done_in : std_logic;
+	signal read_en_out : std_logic;
+	signal rd_ready_in : std_logic;
+	signal data_inout : std_logic_vector((Num_Bytes_in_Word*Num_Bits_in_Byte)-1 downto 0);
 	
 	Begin
+		IF_stage: fetch_stage port map (clock, fetch_enable, address_if, read_en_if, rd_ready_if, data_if,
+										pc_in, instruction_if, npc_if);
+										
+		ID_stage: instruction_decode port map (	instruction=>instruction_if, wb_addr=>wb_address, wb_val=>wb_data,
+												en=>decode_en, wb_en=>wb_en, clock=>clock, command=>op_code, d_register=>d_reg, 
+												shift=>shift, Address=>address, s_register=>s_reg, t_register=>t_reg, immediate=>imm);
+												
+		EX_stage: execute port map (	enable=>execute_en, clock=>clock, op_code=>op_code,d_register=>d_reg, shift=>shift,
+										address=>address, s_register=>s_reg, t_register=>t_reg, immediate=>imm, ALU_output=>alu_out,
+										B_operand=>b_operand, branch_cond=>branch_cond);
+										
+		MEM_stage: mem port map (	enable=>mem_en, CLK=>clock, B=>b_operand, ALU_Output=>alu_out, DATA_MEMORY=>data_mem, READ_READY=>rd_ready_mem, 
+									WRITE_DONE=>wr_done_mem, COND=>branch_cond, NPC=>npc_if, INSTRUCTION=>op_code,
+									PC=>npc_mem, LMD=>mem_output, READ_EN=>read_en_mem, WRITE_EN=>write_en_mem, WORD_BYTE_MEM=>wordbyte_mem, ADDRESS_MEM=>address_mem
+									);
+									
+		WB_stage: writeback_stage port map (	reg_enable=>wb_en, clock=>clock, from_mem=>alu_out, from_alu=>b_operand, instruction=>instruction_if,
+												reg_address=>reg_address, write_data=>write_data, write_back_en=>write_back_enable);
+												
+		memory_module : main_memory port map ( 	clock, address_out, word_byte_out, write_en_out, wr_done_in, read_en_out, rd_ready_in,
+												data_inout, init_signal, dump_signal); 
+												
+		mem_con : mem_controller port map (	clock, address_if, read_en_if, rd_ready_if, data_if, address_mem, wordbyte_mem,
+											write_en_mem, read_en_mem, wr_done_mem, rd_ready_mem, data_mem, 
+											address_out, word_byte_out, write_en_out, wr_done_in, read_en_out,
+											rd_ready_in, data_inout);
 	
 End implementation;
--- Processor Part 2/writeback_stage.vhd
+++ Processor Part 2/writeback_stage.vhd
@@ -16,13 +16,14 @@ Entity writeback_stage is
 	);
 	Port (
 		clock : in std_logic;
+		write_back_en : in std_logic;
 		from_mem : in std_logic_vector (31 downto 0);
 		from_alu : in std_logic_vector (31 downto 0);
 		instruction: in std_logic_vector (31 downto 0);
 		
 		write_data : out std_logic_vector (31 downto 0);
-		reg_address : out integer range 0 to 31;
-		reg_enable : in std_logic
+		reg_address : out std_logic_vector (4 downto 0);
+		reg_enable : out std_logic
 	);
 End writeback_stage;
 
@@ -32,33 +33,51 @@ Architecture implementation of writeback_stage is
 	writeback_process : process(clock)
 	Begin
 		if (clock = '1' and clock'event) then
-		
-			--Set the data to be written
-			if (instruction(31 downto 26) = "010100" OR		--If load word instruction,
-			    instruction(31 downto 26) = "010101") then	--Or if load byte instruction
-				write_data <=from_mem;	--Write to registers from Memory
-			else
-				write_data<= from_alu;	--Write to registers from ALU
-			end if;
-			
-			--Set which register to write to
-			if (instruction(31 downto 26) = "000010" OR
-				instruction(31 downto 26) = "000110" OR
-				instruction(31 downto 26) = "001011" OR
-				instruction(31 downto 26) = "001100" OR
-				instruction(31 downto 26) = "001101" OR
-				instruction(31 downto 26) = "010000" OR
-				instruction(31 downto 26) = "010100" OR
-				instruction(31 downto 26) = "010101") then
-				reg_address<=to_integer(unsigned(instruction(20 downto 16)));	--Store in register t
-			
-			elsif (	instruction(31 downto 26) = "010001" OR
-					instruction(31 downto 26) = "010010" OR
-					instruction(31 downto 26) = "010011") then
-				reg_address<=to_integer(unsigned(instruction(25 downto 21)));	--Store in register S
-			
-			else 
-				reg_address<=to_integer(unsigned(instruction(15 downto 11)));	--Store in register D
+			if (write_back_en = '1') then
+				--Set write-back enable or not
+				if (instruction(31 downto 26) = "000011" OR
+					instruction(31 downto 26) = "000100" OR
+					instruction(31 downto 26) = "010110" OR
+					instruction(31 downto 26) = "010111" OR
+					instruction(31 downto 26) = "011000" OR
+					instruction(31 downto 26) = "011001" OR
+					instruction(31 downto 26) = "011010" OR
+					instruction(31 downto 26) = "011011") then
+					reg_enable <= '0';
+				else
+					reg_enable <= '1';
+				end if;
+				
+				--Set the data to be written
+				if (instruction(31 downto 26) = "010100" OR		--If load word instruction,
+					instruction(31 downto 26) = "010101" OR		--Or if load byte instruction
+					instruction(31 downto 26) = "011100") then	--Or if jump and link instruction
+					write_data <=from_mem;	--Write to registers from Memory
+				else
+					write_data<= from_alu;	--Write to registers from ALU
+				end if;
+				
+				--Set which register to write to
+				if (instruction(31 downto 26) = "011100") then
+					reg_address<="11111";
+				elsif (instruction(31 downto 26) = "000010" OR
+					instruction(31 downto 26) = "000110" OR
+					instruction(31 downto 26) = "001011" OR
+					instruction(31 downto 26) = "001100" OR
+					instruction(31 downto 26) = "001101" OR
+					instruction(31 downto 26) = "010000" OR
+					instruction(31 downto 26) = "010100" OR
+					instruction(31 downto 26) = "010101") then
+					reg_address<=instruction(20 downto 16);	--Store in register t
+				
+				elsif (	instruction(31 downto 26) = "010001" OR
+						instruction(31 downto 26) = "010010" OR
+						instruction(31 downto 26) = "010011") then
+					reg_address<=instruction(25 downto 21);	--Store in register S
+				
+				else 
+					reg_address<=instruction(15 downto 11);	--Store in register D
+				end if;
 			end if;
 		end if;
 	End process;
