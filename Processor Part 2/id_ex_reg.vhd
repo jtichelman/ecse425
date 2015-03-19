43359a114f1cc3ca470ba0e37ea36d3db489fa79
Jeffrey Tichelman <jeffrey.tichelman@mail.mcgill.ca>
2015-03-19 12:08:23 -0400

    wrote first two pipeline registers

---
+++ Processor Part 2/id_ex_reg.vhd
@@ -0,0 +1,48 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+entity id_ex_reg is
+	PORT (	CLK : in std_logic;
+	
+			-- Outputs of ID
+			command_in : in std_logic_vector(5 downto 0);
+			d_reg_in : in std_logic_vector(4 downto 0);
+			shift_in : in std_logic_vector(4 downto 0);
+			address_in : in std_logic_vector(25 downto 0);
+			s_reg_in: in std_logic_vector(31 downto 0);
+			t_reg_in : in std_logic_vector(31 downto 0);
+			imm_in : in std_logic_vector(31 downto 0);
+			
+			-- Inputs to EX
+			op_code : out std_logic_vector(3 downto 0);
+			d_reg_out : out std_logic_vector(4 downto 0);
+			shift_out : out std_logic_vector(4 downto 0);
+			address_out : out std_logic_vector(25 downto 0);
+			s_reg_out : out std_logic_vector(31 downto 0);
+			t_reg_out : out std_logic_vector(31 downto 0);
+			imm_out : : out std_logic_vector(31 downto 0);
+			
+			-- Additional message passing
+			npc_in : in integer;
+			npc_out : out integer
+		);
+end id_ex_reg;
+
+architecture behaviour of id_ex_reg is
+	Begin
+		process0 : process(CLK)
+		Begin
+			if(CLK'EVENT and CLK='0') then
+				op_code <= command_in;
+				d_reg_out <= d_reg_in;
+				shift_out <= shift_in;
+				address_out <= address_in;
+				s_reg_out <= s_reg_in;
+				t_reg_out <= t_reg_in;
+				imm_out <= imm_in;
+				
+				npc_out <= npc_in;
+		end process;
+end behaviour;
+			
--- Processor Part 2/if_id_reg.vhd
+++ Processor Part 2/if_id_reg.vhd
@@ -4,10 +4,16 @@ USE ieee.numeric_std.all;
 
 entity if_id_reg is
 	PORT (	CLK : in std_logic;
+	
+			-- IF outputs
 			instruction_in : in std_logic_vector(31 downto 0);
-			npc : in  integer;
+			npc_in : in  integer;
 			
+			-- ID inputs
 			instruction_out : out std_logic_vector(31 downto 0);
+			
+			-- Message passing
+			npc_out : out integer;
 		);
 end if_id_reg;
 
@@ -16,6 +22,7 @@ architecture behaviour of if_id_reg is
 		process0 : process(CLK)
 		Begin
 			if (CLK'EVENT and CLK = '0') then
-				
-	
+				instruction_out <= instruction_in;
+				npc_out <= npc_in;
+		end process;	
 end behaviour;
